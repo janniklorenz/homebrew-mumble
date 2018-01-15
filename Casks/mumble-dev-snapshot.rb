5bb3f4de51ff6c42e6524baa759c6f50b6d1acc5
Jannik Lorenz <dev@janniklorenz.de>
2018-01-15 01:55:00 +0100

    init

---
+++ Casks/mumble-dev-snapshot.rb
@@ -0,0 +1,11 @@
+cask 'mumble-dev-snapshot' do
+  version '1.3.0'
+  sha256 'c816a8037fa0b61e468354ea06139228bc25f829b09b353f309eebbdecab8b09'
+
+  url 'https://dl.mumble.info/Mumble-1.3.0~2569~gd196a4b~snapshot.dmg'
+  name 'Mumble'
+  homepage 'https://wiki.mumble.info/wiki/Main_Page'
+
+  app 'Mumble.app'
+end
+
