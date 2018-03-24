74aebe1dbb9a68b3d20fdba8e630ea0407510f5f
Jannik Lorenz <dev@janniklorenz.de>
2018-03-24 20:09:00 +0100

    update version tag

--- Casks/mumble-dev-snapshot.rb
+++ Casks/mumble-dev-snapshot.rb
@@ -1,5 +1,5 @@
 cask 'mumble-dev-snapshot' do
-  version '1.3.0'
+  version '1.3.0-2717'
   sha256 '0722f15e0bdb4e8af956b3d366c1c8c885e9e354eed936fba1573b79db699e14'
 
   url 'https://dl.mumble.info/Mumble-1.3.0~2717~gb25db3e~snapshot.dmg'
