4fff600c5d373d91720f11e3275e481a6d6200ad
Jannik Lorenz <dev@janniklorenz.de>
2019-04-19 20:04:12 +0200

    update to 1.3.0-rc1

--- Casks/mumble-dev-snapshot.rb
+++ Casks/mumble-dev-snapshot.rb
@@ -1,8 +1,8 @@
 cask 'mumble-dev-snapshot' do
-  version '1.3.0-2717'
-  sha256 '0722f15e0bdb4e8af956b3d366c1c8c885e9e354eed936fba1573b79db699e14'
+  version '1.3.0-rc1'
+  sha256 '6ba2cf7ea2adea5c659b8bee86966726b7ba69a032d7e89fb52ed6ee2b53a151'
 
-  url 'https://dl.mumble.info/Mumble-1.3.0~2717~gb25db3e~snapshot.dmg'
+  url 'https://dl.mumble.info/Mumble-1.3.0-rc1.dmg'
   name 'Mumble'
   homepage 'https://wiki.mumble.info/wiki/Main_Page'
 
